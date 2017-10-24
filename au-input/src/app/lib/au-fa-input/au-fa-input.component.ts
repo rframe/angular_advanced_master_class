9bdafa33eef42e6e2d10a27e5496b43d31119fca
Russell F <rframe83@gmail.com>
2017-10-24 11:08:00 -0700

    Our First Angular Library - The Font Awesome And Material Design Input Boxes: The ContentChild Decorator, How Does It Work? Component Design Best Practices

--- au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
+++ au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
@@ -1,12 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterContentInit, Component, ContentChild, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'au-fa-input',
   templateUrl: './au-fa-input.component.html',
   styleUrls: ['./au-fa-input.component.css']
 })
-export class AuFaInputComponent implements OnInit {
+export class AuFaInputComponent implements OnInit, AfterContentInit {
+  ngAfterContentInit(): void {
+    console.log('input', this.input);
+  }
+
   @Input() icon: string;
+
+  @ContentChild('input') input: HTMLInputElement;
+
   get classes() {
     const cssClasses = {
       'fa': !!this.icon,
