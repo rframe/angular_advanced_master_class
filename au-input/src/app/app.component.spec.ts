8d689520ded6a9d76cf02624f091e7f3dc13963a
Russell F <rframe83@gmail.com>
2017-10-26 13:17:42 -0700

    Styling Angular Components - Best Practices: Adding Tests the Font Awesome Input - How to Test a Component

--- au-input/src/app/app.component.spec.ts
+++ au-input/src/app/app.component.spec.ts
@@ -1,10 +1,15 @@
-import {TestBed, async} from '@angular/core/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 
 import {AppComponent} from './app.component';
 import {AuFaInputComponent} from "./lib/au-fa-input/au-fa-input.component";
 import {InputRefDirective} from "./lib/common/input-ref.directive";
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('AppComponent', () => {
+  let component: AuFaInputComponent,
+    fixture: ComponentFixture<AppComponent>,
+    el: DebugElement;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -15,11 +20,22 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+    el = fixture.debugElement;
+    fixture.detectChanges();
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   }));
 
 
+  it('should create a font awesome email input', async()=>{
+    const emailField = el.query(By.css('#email-field'));
+    expect(emailField).toBeTruthy();
+    expect(emailField.query(By.css('i.icon.fa.fa-envelope'))).toBeTruthy();
+    expect(emailField.query(By.css('input.test-class'))).toBeTruthy();
+  })
 });
