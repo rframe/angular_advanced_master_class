e6fe1bf98c7ffdd9cf41418d76aa599618075184
russell f <russell.frame@machiningcloud.com>
2017-11-06 11:22:13 -0800

    Writing a Tab Container Component: Content Projection With @ContentChildren and The AfterContentInit Lifecycle Hook

--- au-tab-panel/src/app/au-tab/au-tab.component.ts
+++ au-tab-panel/src/app/au-tab/au-tab.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'au-tab',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuTabComponent implements OnInit {
 
+  @Input() title: string;
   constructor() { }
 
   ngOnInit() {
