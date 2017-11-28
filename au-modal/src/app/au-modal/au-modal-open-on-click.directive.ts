470732a9ffba9878dfd84b672361b43f65e04dfd
russell f <russell.frame@machiningcloud.com>
2017-11-27 16:14:46 -0800

    Modal Component and Structural Directives: Step-By-Step Implementation of a Structural Directive - Learn ViewContainerRef

--- au-modal/src/app/au-modal/au-modal-open-on-click.directive.ts
+++ au-modal/src/app/au-modal/au-modal-open-on-click.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import {Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
 
 @Directive({
   selector: '[auModalOpenOnClick]'
 })
 export class AuModalOpenOnClickDirective {
 
-  constructor() { }
+  @Input() set auModalOpenOnClick(element: HTMLBaseElement | Array<HTMLBaseElement>) {
+    let elements: Array<HTMLBaseElement> = [];
+
+    if (element instanceof Array) {
+      elements = [...element];
+    } else {
+      elements = [element];
+    }
+
+    elements.forEach((el) => {
+      el.addEventListener('click', () => {
+        this.viewContainer.clear();
+        this.viewContainer.createEmbeddedView(this.templateRef);
+      })
+    })
+  }
+
+  constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) {
+  }
 
 }
