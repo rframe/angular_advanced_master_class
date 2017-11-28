b67ddb4e33c6c013526ee4ac0039f42f0e7192c1
russell f <russell.frame@machiningcloud.com>
2017-11-27 16:29:21 -0800

    Modal Component and Structural Directives: Running Into A Design Issue While Implementing The Modal Close Functionality

--- au-modal/src/app/au-modal/au-modal-open-on-click.directive.ts
+++ au-modal/src/app/au-modal/au-modal-open-on-click.directive.ts
@@ -1,11 +1,21 @@
-import {Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
+import {AfterContentInit, ContentChild, Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
+import {AuModalComponent} from './au-modal.component';
 
 @Directive({
   selector: '[auModalOpenOnClick]'
 })
 export class AuModalOpenOnClickDirective {
 
-  @Input() set auModalOpenOnClick(element: HTMLBaseElement | Array<HTMLBaseElement>) {
+  @Input() set auModalOpenOnClick (element: HTMLBaseElement | Array<HTMLBaseElement>) {
+    this.setAuModalOpenOnClick(element);
+  }
+
+  @ContentChild(AuModalComponent) modal: AuModalComponent;
+
+  constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) {
+  }
+
+  setAuModalOpenOnClick(element: HTMLBaseElement | Array<HTMLBaseElement>) {
     let elements: Array<HTMLBaseElement> = [];
 
     if (element instanceof Array) {
@@ -20,9 +30,6 @@ export class AuModalOpenOnClickDirective {
         this.viewContainer.createEmbeddedView(this.templateRef);
       })
     })
-  }
-
-  constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) {
   }
 
 }
