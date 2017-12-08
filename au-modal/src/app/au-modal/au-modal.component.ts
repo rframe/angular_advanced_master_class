f02462d2fe8e05556fcbb637aade37e69886f4d6
russell f <russell.frame@machiningcloud.com>
2017-12-08 08:52:38 -0800

    Modal Component and Structural Directives: Angular Global Keyboard Handling With EventManager

--- au-modal/src/app/au-modal/au-modal.component.ts
+++ au-modal/src/app/au-modal/au-modal.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit, TemplateRef, ViewEncapsulation} from '@angular/core';
 import {AuModalService} from './modal.service';
+import {EventManager} from '@angular/platform-browser';
 
 @Component({
   selector: 'au-modal',
@@ -9,14 +10,29 @@ import {AuModalService} from './modal.service';
 })
 export class AuModalComponent implements OnInit {
   @Input() body: TemplateRef<any>;
-  constructor(private modalService: AuModalService) { }
+  @Input() hideOnEscape: boolean = true;
+  @Input() hideOnClickOutside: boolean = true;
+  constructor(private modalService: AuModalService,
+              private eventManager: EventManager) { }
 
   ngOnInit() {
+    this.eventManager.addGlobalEventListener('window', 'keyup.esc', () => {
+      if(this.hideOnEscape) {
+        this.closeModal();
+      }
+    });
   }
 
   closeModal() {
     this.modalService.close();
   }
+
+  onClickOutsideModal() {
+    if(this.hideOnClickOutside) {
+      this.closeModal();
+    }
+  }
+
   cancelClick(event: KeyboardEvent) {
     event.preventDefault();
     event.stopPropagation();
